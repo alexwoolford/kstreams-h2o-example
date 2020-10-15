5a4ac6f86e1bd37ef2b227fdd6e288267a10714b
Alex Woolford <alex@woolford.io>
2020-10-15 13:47:44 -0600

    minor refactor for Confluent Cloud

--- kstreams-iris-classification/src/main/java/io/woolford/kstreams/h2o/example/KafkaConstants.java
+++
@@ -1,8 +0,0 @@
-package io.woolford.kstreams.h2o.example;
-
-public interface KafkaConstants {
-
-    public static String KAFKA_BROKERS = "cp01.woolford.io:9092,cp02.woolford.io:9092,cp03.woolford.io:9092";
-    public static String APPLICATION_ID = "kstreams-h2o-example";
-
-}
