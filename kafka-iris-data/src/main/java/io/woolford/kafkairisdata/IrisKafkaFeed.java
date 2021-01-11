4cef7be1d65911be23d56d1a7cbd7af037eac2db
Alex Woolford <alex@woolford.io>
2021-01-10 18:07:07 -0700

    kafka-iris-data runnable as standalone jar

--- kafka-iris-data/src/main/java/io/woolford/kafkairisdata/IrisKafkaFeed.java
+++ kafka-iris-data/src/main/java/io/woolford/kafkairisdata/IrisKafkaFeed.java
@@ -6,21 +6,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Random;
 
 
 @Component
 public class IrisKafkaFeed {
+
+    private final Logger logger = LoggerFactory.getLogger(IrisKafkaFeed.class);
 
     @Autowired
     KafkaTemplate kafkaTemplate;
@@ -29,7 +35,15 @@ public class IrisKafkaFeed {
 
     public IrisKafkaFeed() throws IOException {
 
-        File csvFile = new File(getClass().getClassLoader().getResource("iris.csv").getFile());
+        String irisCsvString = "";
+        ClassPathResource cpr = new ClassPathResource("iris.csv");
+        try {
+            byte[] bdata = FileCopyUtils.copyToByteArray(cpr.getInputStream());
+            irisCsvString = new String(bdata, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            logger.warn("IOException", e);
+        }
+
         CsvSchema schema = CsvSchema.builder()
                 .addColumn("sepalLength", CsvSchema.ColumnType.NUMBER)
                 .addColumn("sepalWidth", CsvSchema.ColumnType.NUMBER)
@@ -39,7 +53,7 @@ public class IrisKafkaFeed {
                 .build().withHeader();
 
         CsvMapper csvMapper = new CsvMapper();
-        MappingIterator<IrisRecord> irisIter = csvMapper.readerFor(IrisRecord.class).with(schema).readValues(csvFile);
+        MappingIterator<IrisRecord> irisIter = csvMapper.readerFor(IrisRecord.class).with(schema).readValues(irisCsvString);
         this.irisRecordList = irisIter.readAll();
 
     }
